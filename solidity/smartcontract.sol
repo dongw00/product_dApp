9963b5784d586cbe854b30504886bf6fd8ff0e75
dongw00 <albadw0428@gmail.com>
2019-07-22 16:52:54 +0900

    fix smartContract

--- solidity/smartcontract.sol
+++ solidity/smartcontract.sol
@@ -1,17 +1,17 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >=0.4.22 < 0.7.0;
 
 contract ProductContract {
-    uint8 numberOfProducts; // 총 제품의 수
+    uint8 total; // 총 제품의 수
 
     struct myStruct {
-        uint   number;
+        uint   id;
         string productName;
         string locaton;
         uint timestamp;
     }
 
     event product (
-        uint number,
+        uint id,
         string productName,
         string location,
         uint timestamp
@@ -20,19 +20,19 @@ contract ProductContract {
     myStruct[] public products;
 
     /* 사용자가 입력한 제품을 등록 */
-    function addProStru (uint _initNumber, string memory _firstString, string memory _secondString) public {
-        products.push(myStruct(_initNumber, _firstString, _secondString, block.timestamp)) -1;
-        numberOfProducts++;
-        emit product(_initNumber, _firstString, _secondString, block.timestamp);
-    }
-
-    /* 제품 등록의 수를 리턴 */
-    function getNumOfProducts() public view returns(uint8) {
-        return numberOfProducts;
+    function addProduct (uint _id, string memory _firstString, string memory _secondString) public {
+        products.push(myStruct(_id, _firstString, _secondString, block.timestamp));
+        total++;
+        emit product(_id, _firstString, _secondString, block.timestamp);
     }
 
     /* 번호에 해당하는 제품의 이름을 리턴 */
-    function getProductStruct(uint _index) public view returns (uint, string memory, string memory, uint) {
-        return (products[_index].number, products[_index].productName, products[_index].locaton, products[_index].timestamp);
+    function getProduct(uint _idx) public view returns (uint, string memory, string memory, uint) {
+        return (products[_idx].id, products[_idx].productName, products[_idx].locaton, products[_idx].timestamp);
+    }
+
+    /* 제품 등록의 수를 리턴 */
+    function getTotal() public view returns(uint8) {
+        return total;
     }
 }
