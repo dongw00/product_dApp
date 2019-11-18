563e60ccdd2c005b7c2d8fab4c894528d32c481d
dongw00 <albadw0428@gmail.com>
2019-11-18 11:40:35 +0900

    fix miner bug

--- js/app.js
+++ js/app.js
@@ -1,48 +1,50 @@
-typeof web3 !== 'undefined'
+typeof web3 !== "undefined"
   ? (web3 = new Web3(web3.currentProvider))
-  : (web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')));
+  : (web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545")));
 
 if (web3.isConnected()) {
-  console.log('connected');
+  console.log("connected");
 } else {
-  console.log('not connected');
+  console.log("not connected");
   exit;
 }
 
-const contractAddress = '0x9f49cdd950453f91515c26823c368a9d394b2c5d';
+const contractAddress = "0xf3c06d383278a1a1f776e994483ab319905e4c3b";
 const smartContract = web3.eth.contract(abi).at(contractAddress);
 
 function init() {
   // 첫번째 계정으로 주소 설정
-  document.getElementById('account').value = web3.eth.accounts[0];
+  $("#account").val(web3.eth.accounts[0]);
+  moment.locale();
 }
 
-function showList() {
-  const table = document.getElementById('table1');
-  const length = smartContract.getTotal();
+async function showList() {
+  const table = document.getElementById("table1");
+  const total = smartContract.getTotal();
 
-  smartContract.product().watch((err, res) => {
+  smartContract.product().watch(async (err, res) => {
     if (!err) {
-      console.dir(res);
       const row = table.insertRow();
       const cell1 = row.insertCell(0);
       const cell2 = row.insertCell(1);
       const cell3 = row.insertCell(2);
       const cell4 = row.insertCell(3);
-      cell1.innerHTML = res.args.id.toString();
-      cell2.innerHTML = res.args.productName;
-      cell3.innerHTML = res.args.location;
-      cell4.style.width = '60%';
-      cell4.innerHTML = new Date(res.args.timestamp.toString() * 1000);
+
+      const time = (await res.args.timestamp.toString()) * 1000;
+      cell1.innerHTML = await res.args.id.toString();
+      cell2.innerHTML = await res.args.productName;
+      cell3.innerHTML = await res.args.location;
+      cell4.innerHTML = moment(time).format("YYYY-MM-DD hh:mm");
     }
   });
 
-  for (let i = 0; i < length; i++) {
-    const product = smartContract.getProduct(i);
-    const toString = product.toString();
-    const strArray = toString.split(',');
+  for (let i = 0; i < total; i++) {
+    const product = await smartContract.getProduct(i);
+    const toString = await product.toString();
+    const strArray = toString.split(",");
 
-    const timestamp = new Date(strArray[3] * 1000);
+    console.dir(product);
+    const time = strArray[3] * 1000;
     const row = table.insertRow();
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
@@ -51,23 +53,19 @@ function showList() {
     cell1.innerHTML = strArray[0];
     cell2.innerHTML = strArray[1];
     cell3.innerHTML = strArray[2];
-    cell4.style.width = '60%';
-    cell4.innerHTML = timestamp;
+    cell4.style.width = "60%";
+    (cell4.innerHTML = moment(time)), format("YYYY-MM-DD hh:mm");
   }
 }
 
 function addProduct() {
-  const pronumber = document.getElementById('pronumber').value;
-  const proname = document.getElementById('proname').value;
-  const proloc = document.getElementById('proloc').value;
-  const account = document.getElementById('account').value;
   smartContract.addProduct(
-    pronumber,
-    proname,
-    proloc,
+    $("#account").val(),
+    $("#proname").val(),
+    $("#proloc").val(),
     { from: account, gas: 3000000 },
     (err, result) => {
-      if (!err) alert('트랜잭션이 성공적으로 전송되었습니다.\n' + result);
+      if (!err) alert("트랜잭션이 성공적으로 전송되었습니다.\n" + result);
     }
   );
 }
--- solidity/smartcontract.sol
+++ solidity/smartcontract.sol
@@ -1,8 +1,7 @@
 pragma solidity >=0.4.22 < 0.7.0;
 
 contract ProductContract {
-    uint8 total; // 총 제품의 수
-
+    uint total;
     struct myStruct {
         uint   id;
         string productName;
@@ -20,10 +19,9 @@ contract ProductContract {
     myStruct[] public products;
 
     /* 사용자가 입력한 제품을 등록 */
-    function addProduct (uint _id, string memory _firstString, string memory _secondString) public {
-        products.push(myStruct(_id, _firstString, _secondString, block.timestamp));
-        total++;
-        emit product(_id, _firstString, _secondString, block.timestamp);
+    function addProduct (uint _id, string memory _productName, string memory _location) public {
+        products.push(myStruct(_id, _productName, _location, block.timestamp));
+        emit product(_id, _productName, _location, block.timestamp);
     }
 
     /* 번호에 해당하는 제품의 이름을 리턴 */
@@ -31,8 +29,7 @@ contract ProductContract {
         return (products[_idx].id, products[_idx].productName, products[_idx].locaton, products[_idx].timestamp);
     }
 
-    /* 제품 등록의 수를 리턴 */
-    function getTotal() public view returns(uint8) {
+    function getTotal() public view returns (uint) {
         return total;
     }
 }
